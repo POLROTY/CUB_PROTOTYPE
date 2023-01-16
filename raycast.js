4decd4ec1306e2a33b37d8ba1b93df17aa201a6b
rpol <polrotyuni@gmail.com>
2023-01-17 00:51:16 +0100

    2D map with player movement and wall collision

--- raycast.js
+++ raycast.js
@@ -23,6 +23,15 @@ class Map {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         ];
     }
+
+    is_it_lava(x, y) {
+        if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) {
+            return true;
+        }
+        var mapGridIndexX = Math.floor(x / TILE_SIZE);
+        var mapGridIndexY = Math.floor(y / TILE_SIZE);
+        return this.grid[mapGridIndexY][mapGridIndexX] != 0;
+    }
     // This method renders the map on the canvas
     render() {
         // This nested loop iterates through every element of the grid
@@ -41,7 +50,71 @@ class Map {
     }
 }
 
+// class to animate and update player location
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        this.radius = 3;
+        this.turnDirection = 0; // -1 if left, +1 if right
+        this.walkDirection = 0; // -1 if back, +1 if front
+        this.rotationAngle = Math.PI / 2;
+        this.moveSpeed = 1.0;
+        this.rotationSpeed = 1 * (Math.PI / 180);
+    }
+    update() {
+        this.rotationAngle += this.turnDirection * this.rotationSpeed;
+
+        var moveStep = this.walkDirection * this.moveSpeed;
+        var newX = (this.x + Math.cos(this.rotationAngle) * moveStep);
+        var newY = (this.y + Math.sin(this.rotationAngle) * moveStep);
+        if (!grid.is_it_lava(newX,newY)) {
+            this.x = newX;
+            this.y = newY;
+        }
+    }
+    render() {
+        noStroke();
+        fill("red");
+        circle(this.x, this.y, this.radius);
+        stroke("red");
+        line(
+            this.x,
+            this.y,
+            this.x + Math.cos(this.rotationAngle) * 42,
+            this.y + Math.sin(this.rotationAngle) * 42
+        );
+    }
+}
+
 var grid = new Map(); // Create a new map object
+var player = new Player();
+
+//from p5 lib for key press events
+function keyPressed() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = +1;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = -1;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = +1;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = -1;
+    }
+}
+
+//from p5 lib for key release events
+function keyReleased() {
+    if (keyCode == UP_ARROW) {
+        player.walkDirection = 0;
+    } else if (keyCode == DOWN_ARROW) {
+        player.walkDirection = 0;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.turnDirection = 0;
+    } else if (keyCode == LEFT_ARROW) {
+        player.turnDirection = 0;
+    }
+}
 
 // This function sets up the canvas
 function setup() {
@@ -50,7 +123,7 @@ function setup() {
 
 // This function is called before each frame is rendered and can be used to update game objects
 function update() {
-    // TODO: update all game objects before we render the next frame
+    player.update();
 }
 
 // This function is called on every frame to render the map on the canvas
@@ -58,4 +131,5 @@ function draw() {
     update(); // Call the update function before rendering
 
     grid.render(); // Call the render method on the map object to render the map on the canvas
+    player.render(); // Call the render method on the player object to render the player on the canvas
 }
